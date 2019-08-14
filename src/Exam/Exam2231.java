9a3e0681a3c017cc62f735b527fc0576b062f1e0
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2019-08-14 17:23:54 +0900

    Create Exam2231.java

---
+++ src/Exam/Exam2231.java
@@ -0,0 +1,32 @@
+package Exam;
+
+import java.util.Scanner;
+
+public class Exam2231 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		int a = num;
+		int res = 0;
+		while (num != 0) {
+			num--;
+
+			int temp = num;
+			int temp2 = num;
+			while (temp != 0) {
+				temp2 += temp % 10;
+				temp /= 10;
+			}
+			if (temp2 == a) {
+				res = num;
+			}
+
+		}
+
+		System.out.println(res);
+
+	}
+}
