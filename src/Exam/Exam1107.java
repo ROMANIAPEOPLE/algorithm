6f7358775e566a04600567a6d09d201f55955b7f
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2019-09-26 23:00:29 +0900

    55

--- src/Exam/Exam1107.java
+++ src/Exam/Exam1107.java
@@ -1,10 +1,11 @@
-package Exam;
+ package Exam;
 
 import java.util.*;
 
 public class Exam1107 {
 	static boolean[] broken = new boolean[10];
-
+	//true => 고장 
+	//false =>노고장
 	static int possible(int c) {
 		if (c == 0) {
 			if (broken[0]) {
@@ -26,15 +27,15 @@ public class Exam1107 {
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+		int n = sc.nextInt(); //이동하려는 채널
+		int m = sc.nextInt(); //고장난 버튼 개수
 		for (int i = 0; i < m; i++) {
-			int x = sc.nextInt();
+			int x = sc.nextInt(); //고장난 버튼 추가
 			broken[x] = true;
 		}
 		int ans = n - 100;
 		if (ans < 0) {
-			ans = -ans;
+			ans = -ans;  // +,-뺴고 모든 리모컨의 키가 고장났을때를 초기값으로 설정한다.
 		}
 		for (int i = 0; i <= 1000000; i++) {
 			int c = i;
