e89dbd7f87d5af5ae695b8b60da127ac908bc37a
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2019-11-05 14:27:36 +0900

    1

--- src/Exam/Exam14501.java
+++ src/Exam/Exam14501.java
@@ -4,40 +4,41 @@ import java.util.Scanner;
 
 public class Exam14501 {
 
-	static int [] T; //일 수
-	static int [] P; // 페이
-	static int ans=Integer.MIN_VALUE;
-	static int n;
+	static int ans = Integer.MIN_VALUE;
 	
-	static void go(int day, int sum) {
-		if(day==n+1) {
+	static void go(int day, int sum, int goal ,int [] T, int [] P) {
+		if(day == goal) {
 			if(ans < sum) {
-				ans=sum;
+				ans = sum;
+				return;
 			}
+		}
+		
+		if(day > goal) {
 			return;
 		}
-		if(day>n+1) {
-			return;
-		}
-		go(day+1,sum);
-		go(day+T[day],sum+P[day]);
+		
+		go(day+1,sum,goal,T,P);
+		go(day+T[day], sum+P[day],goal,T,P);
+		
 	}
 	
 	public static void main(String[] args) {
-		
 		Scanner sc= new Scanner(System.in);
-		n=sc.nextInt();
-		T=new int[n+1];
-		P=new int[n+1];
+		int N =sc.nextInt();
 		
-		for(int i=1; i<=n; i++) {
-			T[i]= sc.nextInt();
-			P[i]= sc.nextInt();
+		int [] T = new int[N+1];  //소요되는 기간
+		int [] P = new int[N+1]; //페이
+		
+		for(int i=1; i<=N; i++) {
+			T[i]=sc.nextInt();
+			P[i]=sc.nextInt();
 		}
-		
-		go(1,0);
+
+		go(1,0,N+1,T,P);
 		System.out.println(ans);
 		
+		
+		
 	}
-	
 }
--- src/Exam/Exam14889_2.java
+++ src/Exam/Exam14889_2.java
@@ -4,63 +4,56 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Exam14889 {
+public class Exam14889_2 {
 
-	static boolean [] check = new boolean[10]; //0~9까지 check
+	static boolean [] check = new boolean[10]; // 0~9까지 사용 여부 판단
+	static char [] a = new char[10]; //부등호를 담을 배열
 	static int n;
-	static char [] a = new char[10]; // 부등호는 최대 9개임
 	static ArrayList<String> ans = new ArrayList<>();
+	
 	
 	static boolean ck(char a, char b, char c) {
 		if(c == '<') {
-			if(a>b) {
-				return false;
-			}
+			if(a>b) return false;
 		}
 		if(c == '>') {
-			if(a<b) {
-				return false;
-			}
+			if(a<b) return false;
 		}
 		return true;
+		
 	}
 	
-	
 	static void go(int index, String num) {
+		
 		if(index == n+1) {
 			ans.add(num);
 			return;
 		}
 		
-		
 		for(int i=0; i<=9; i++) {
 			if(check[i]) continue;
-			if(index == 0 || ck(num.charAt(index-1) ,(char) (i+'0') , a[index-1])) {
-				check[i]=true;
-				go(index+1, num+Integer.toString(i));
-				check[i]= false;
+			if(index == 0 || ck(num.charAt(index-1) , (char)(i+'0'), a[index-1] )) {
+				check[i]= true;
+				go(index+1,num+Integer.toString(i));
+				check[i] = false;
 			}
 		}
-		
 	}
 	
+	
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-		n=sc.nextInt();
-		
+		n = sc.nextInt();
 		for(int i=0; i<n; i++) {
-			a[i] = sc.next().toCharArray()[0];
+			a[i]=sc.next().toCharArray()[0];
 		}
 		
 		go(0,"");
 		Collections.sort(ans);
-		
 		System.out.println(ans.get(ans.size()-1));
 		System.out.println(ans.get(0));
 		
 		
 		
 	}
-
 }
