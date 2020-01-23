d13b040cd0902cadb0214a348c5ee4ef377f1586
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-23 21:46:42 +0900

    백준 1181

---
+++ src/Exam/Exam1181.java
@@ -0,0 +1,42 @@
+package Exam;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class Exam1181 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		HashSet<String> set = new HashSet<>();
+		// set은 중복을 허용하지 않기때문에 중복 제거를 위해 set을 사용.
+		for (int i = 0; i < N; i++) {
+			set.add(br.readLine());
+		}
+
+		ArrayList<String> list = new ArrayList<>(set);
+
+		Collections.sort(list, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				if (s1.length() > s2.length())
+					return 1;
+				else if (s1.length() < s2.length())
+					return -1;
+				else
+					return s1.compareTo(s2);
+			}
+		});
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+
+	}
+
+}
