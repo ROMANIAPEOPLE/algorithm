05693f08757a82d1d921574465033ec42b7e26a8
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-24 19:53:38 +0900

    Update Exam1181.java

--- src/Exam/Exam1181.java
+++ src/Exam/Exam1181.java
@@ -12,15 +12,12 @@ public class Exam1181 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-
 		HashSet<String> set = new HashSet<>();
 		// set은 중복을 허용하지 않기때문에 중복 제거를 위해 set을 사용.
 		for (int i = 0; i < N; i++) {
 			set.add(br.readLine());
 		}
-
 		ArrayList<String> list = new ArrayList<>(set);
-
 		Collections.sort(list, new Comparator<String>() {
 			@Override
 			public int compare(String s1, String s2) {
@@ -32,11 +29,11 @@ public class Exam1181 {
 					return s1.compareTo(s2);
 			}
 		});
-
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i));
 		}
-
+		
+		Integer i =null;
+		
 	}
-
 }
