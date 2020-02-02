b7394960664f77e0c96cbb7d6271b1ac7fc295d8
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-02-02 23:52:39 +0900

    Update Exam14503.java

--- src/Exam/Exam14503.java
+++ src/Exam/Exam14503.java
@@ -1,4 +1,7 @@
 package Exam;
+
+import java.util.Scanner;
+
 /*
  * ºÏ=0
  * µ¿=1
@@ -10,6 +13,13 @@ public class Exam14503 {
 	static int [] dx = {-1, 0, 1, 0};
 	static int [] dy = {0, 1, 0, -1};
 	
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
 	
 	
 }
