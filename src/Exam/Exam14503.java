15c86628e5c311f2a3af5973bb77d1eb0bd92174
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-02-02 23:51:48 +0900

    Create Exam14503.java

---
+++ src/Exam/Exam14503.java
@@ -0,0 +1,15 @@
+package Exam;
+/*
+ * ºÏ=0
+ * µ¿=1
+ * ³²=2
+ * ¼­=3
+ */
+public class Exam14503 {
+	static int [][] map;
+	static int [] dx = {-1, 0, 1, 0};
+	static int [] dy = {0, 1, 0, -1};
+	
+	
+	
+}
