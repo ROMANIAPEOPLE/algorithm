1d7e4a5a5a00d610e24a51e1235e09b524a9f983
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-07-09 23:38:29 +0900

    Update 키패드누르기_다시.java

--- src/프로그래머스/키패드누르기_다시.java
+++ src/프로그래머스/키패드누르기_다시.java
@@ -3,69 +3,68 @@ package 프로그래머스;
 public class 키패드누르기_다시 {
 
 	public static void main(String[] args) {
-		int[] numbers = { 7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2 };
-		String hand = "left";
+		int[] numbers = { 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 };
+		String hand = "right";
 
 		System.out.println(solution(numbers, hand));
 
 	}
-	public static String solution(int [] n, String hand) {
-		int [] phone = {'1','2','3','4','5','6','7','8','9','*','0','#'};
-		int posL=9;
-		int posR=11;
-		
+
+	public static String solution(int[] n, String hand) {
 		StringBuilder bd = new StringBuilder();
-		
-		for(int number : n) {
-			if(number == '1' || number == '4' || number == '7') {
-				posL = number;
+
+		int leftLocation = 10;
+		int rightLocation = 12;
+
+		for (int number : n) {
+			if (number == 1 || number == 4 || number == 7) {
 				bd.append("L");
-			}
-			else if(number == '3' || number == '6' || number == '9') {
-				posR = number;
+				leftLocation = number;
+			} else if (number == 3 || number == 6 || number == 9) {
 				bd.append("R");
-			}
-			else {
-				if(getXY(posL,posR,number,hand) == "R") {
-					posR = number;
+				rightLocation = number;
+			} else { // 2 5 8 0
+				int distanceL = getDist(leftLocation, number);
+				int distanceR = getDist(rightLocation, number);
+
+				if (distanceL > distanceR) {
 					bd.append("R");
-				}else {
-					posL = number;
+					rightLocation = number;
+				} else if (distanceL < distanceR) {
 					bd.append("L");
+					leftLocation = number;
+				} else {
+					if (hand.equals("right")) {
+						bd.append("R");
+						rightLocation = number;
+					} else {
+						bd.append("L");
+						leftLocation = number;
+					}
 				}
+
 			}
-			
 		}
 		return bd.toString();
 	}
-	public static String getXY(int posL, int posR , int number,String hand) {
-		int numX = (number-1) /3;
-		int numY = (number-1) %3;
-		
-		int leftX = (posL-1) /3;
-		int leftY = (posL-1) %3;
-		
-		int rX = (posR-1) /3;
-		int rY = (posR-1) %3;
-		
-		int leftValue = (int) Math.abs((Math.pow(leftX-numX, 2) + Math.pow(leftY-numY,2)));
-		int rValue = (int) Math.abs((Math.pow(rX-numX, 2) + Math.pow(rY-numY,2)));
-		
-		if(leftValue > rValue) {
-			return "R";
+
+	public static int getDist(int location, int number) {
+
+		if (number == 0) {
+			number = 11;
 		}
-		else if(leftValue < rValue) {
-			return "L";
+
+		if (location == 0) {
+			location = 11;
 		}
-		else {
-			if(hand.equals("right")) {
-				return "R";
-			}else {
-				return "L";
-			}
-			
-		}
-		
-		
+
+		int locationX = (location - 1) / 3;
+		int locationY = (location - 1) % 3;
+
+		int numberX = (number - 1) / 3;
+		int numberY = (number - 1) % 3;
+
+		return Math.abs(locationX - numberX) + Math.abs(locationY - numberY);
+
 	}
 }
